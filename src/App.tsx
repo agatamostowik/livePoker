b3e28e97d9a07ee8febbbc5e4a46df07512f8049
Agata Mostowik <agatamostowik@gmail.com>
2023-03-31 12:29:38 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@ import { useRef } from "react";
 import { useMeasure } from "react-use";
 
 function App() {
-  const canvasRef = useRef<HTMLDivElement>(null);
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  const [_ref, { x, y, width, height, top, right, bottom, left }] =
-    useMeasure();
+  const [containerRef, { width }] = useMeasure<HTMLDivElement>();
 
   const getVideo = () => {
-    navigator.mediaDevices
-      .getUserMedia({ video: { width }, audio: false })
+    window.navigator.mediaDevices
+      .getUserMedia({ video: { width: 1000 }, audio: false })
       .then((stream) => {
         videoRef.current!.srcObject = stream;
         videoRef.current!.play();
@@ -21,12 +19,12 @@ function App() {
   };
 
   return (
-    <div className="canvas" ref={canvasRef}>
+    <div ref={containerRef}>
       <button className="button" onClick={getVideo}>
         getVideo
       </button>
       <video className="video" ref={videoRef} />
-      <canvas />
+      {/* <canvas /> */}
     </div>
   );
 }
